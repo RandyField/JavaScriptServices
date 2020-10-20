755a8d8efd3b3658a7f740ebaaab18c29edce680
randyfield <287572291@qq.com>
2020-10-20 15:30:03 +0800

    support vue

--- src/Microsoft.AspNetCore.SpaServices.Extensions/Npm/NpmScriptRunner.cs
+++ src/Microsoft.AspNetCore.SpaServices.Extensions/Npm/NpmScriptRunner.cs
@@ -23,6 +23,46 @@ namespace Microsoft.AspNetCore.NodeServices.Npm
 
         private static Regex AnsiColorRegex = new Regex("\x001b\\[[0-9;]*m", RegexOptions.None, TimeSpan.FromSeconds(1));
 
+        public NpmScriptRunner(string workingDirectory, string scriptName, string arguments)
+        {
+            if (string.IsNullOrEmpty(workingDirectory))
+            {
+                throw new ArgumentException("Cannot be null or empty.", nameof(workingDirectory));
+            }
+
+            if (string.IsNullOrEmpty(scriptName))
+            {
+                throw new ArgumentException("Cannot be null or empty.", nameof(scriptName));
+            }
+
+            var npmExe = "npm";
+            var completeArguments = $"run {scriptName} {arguments ?? string.Empty}";
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                // On Windows, the NPM executable is a .cmd file, so it can't be executed
+                // directly (except with UseShellExecute=true, but that's no good, because
+                // it prevents capturing stdio). So we need to invoke it via "cmd /c".
+                npmExe = "cmd";
+                completeArguments = $"/c npm {completeArguments}";
+            }
+
+            var processStartInfo = new ProcessStartInfo(npmExe)
+            {
+                Arguments = completeArguments,
+                UseShellExecute = false,
+                RedirectStandardInput = true,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                WorkingDirectory = workingDirectory
+            };
+
+            var process = LaunchNodeProcess(processStartInfo);
+            StdOut = new EventedStreamReader(process.StandardOutput);
+            //StdOut = new EventedStreamReader(process.StandardOutput);
+
+            StdErr = new EventedStreamReader(process.StandardError);
+        }
+
         public NpmScriptRunner(string workingDirectory, string scriptName, string arguments, IDictionary<string, string> envVars)
         {
             if (string.IsNullOrEmpty(workingDirectory))
